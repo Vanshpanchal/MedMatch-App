8064e68d8f070091c7e7984d57b1b3c93962302a
Vansh Panchal <vansh.panchal719@gmail.com>
2026-03-28 12:51:44 +0530

    Refactor addressApi
    Updated the API URL construction to remove hardcoded API key.

--- app/src/main/java/com/example/med/Home.kt
+++ app/src/main/java/com/example/med/Home.kt
@@ -95,7 +95,7 @@ class Home : AppCompatActivity() {
     private fun addressApi(Address: String, Uid: String) {
         var cordinates = listOf<Double>()
         val url =
-            "https://api.geoapify.com/v1/geocode/search?text=$Address&apiKey=a4df04f3e2154cafbf08d57831558743"
+            "https://api.geoapify.com/v1/geocode/search?text=$Address&apiKey="
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             { response ->
